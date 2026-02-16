cc53886594e1709fbe9e27956428b7ecc19e0ab7
Hércules Lopes <69056516+herculeslopes@users.noreply.github.com>
2026-02-16 15:41:08 -0300

    gh-pages config files

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/quiz-me-on-this',
   plugins: [
     devtools(),
     tanstackRouter({
